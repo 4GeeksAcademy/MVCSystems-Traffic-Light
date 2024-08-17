019647d150af8b367b344f64eea215cfb27329f2
MVCSystems <carlosmerav@gmail.com>
2024-08-17 00:27:30 -0500

    Add

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,23 +3,45 @@ import React, { useState } from "react";
 //create your first component
 const Home = () => {
   const [color, setColor] = useState("");
+  const [colors, setColors] = useState([
+    "bg-danger",
+    "bg-warning",
+    "bg-success",
+  ]);
+  const [index, setIndex] = useState(0);
+  const [purple, setPurple] = useState(false);
 
   const handleClick = (selectedColor) => {
     setColor(selectedColor);
   };
 
+  const handleToggleColor = () => {
+    const nextIndex = (index + 1) % colors.length;
+    setIndex(nextIndex);
+    setColor(colors[nextIndex]);
+  };
+
+  const handlePurple = () => {
+    if (!purple) {
+      setColors([...colors, "bg-purple"]);
+    } else {
+      setColors(colors.filter((c) => c !== "bg-purple"));
+    }
+    setPurple(!purple);
+  };
+
   return (
     <div
-      className="d-flex justify-content-center align-items-center"
+      className="d-flex flex-column justify-content-center align-items-center"
       style={{ height: "100vh" }}
     >
-      <div className="position-relative">
+      <div className="position-relative mb-4">
         <div
           className="bg-black position-absolute"
           style={{ width: "10px", height: "80px", top: "-80px", left: "35px" }}
         ></div>
         <div
-          className="d-flex flex-column align-tems-center justify-content-center align-items-center bg-black rounded"
+          className="d-flex flex-column align-items-center justify-content-center bg-black rounded"
           style={{ width: "80px", height: "200px" }}
         >
           <div
@@ -58,7 +80,38 @@ const Home = () => {
             }}
             onClick={() => handleClick("bg-success")}
           ></div>
+          {purple && (
+            <div
+              className="rounded-circle"
+              style={{
+                width: "60px",
+                height: "60px",
+                backgroundColor: "purple",
+                boxShadow:
+                  color === "bg-purple"
+                    ? "0px 0px 50px 20px rgb(219,241,190)"
+                    : "none",
+              }}
+              onClick={() => handleClick("bg-purple")}
+            ></div>
+          )}
         </div>
+      </div>
+
+      <div className="d-flex flex-column align-items-center space-y-4">
+        <button
+          className="bg-blue-500 text-dark py-2 px-4 rounded hover:bg-blue-600 transition duration-300 mb-2"
+          onClick={handleToggleColor}
+        >
+          Alternar color
+        </button>
+
+        <button
+          className="bg-purple-500 text-dark py-2 px-4 rounded hover:bg-purple-600 transition duration-300"
+          onClick={handlePurple}
+        >
+          {purple ? "Eliminar color púrpura" : "Añadir color púrpura"}
+        </button>
       </div>
     </div>
   );
